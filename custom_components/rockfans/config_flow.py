36a7175fef48fe07d9632091bba8acd0f8044125
msegre <44104105+msegre@users.noreply.github.com>
2026-05-15 11:07:07 -0400

    Update config_flow.py

--- custom_components/rockfans/config_flow.py
+++ custom_components/rockfans/config_flow.py
@@ -7,11 +7,9 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME
 from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN, CONF_DEVICE_ID, CONF_LOCAL_KEY, CONF_IP
-from .tuya_client import TuyaClient
+from .const import DOMAIN, CONF_DEVICE_ID, CONF_LOCAL_KEY, CONF_IP, CONF_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -30,18 +28,19 @@ class TuyaCeilingFanConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-   async def async_step_user(
-    self, user_input: dict[str, Any] | None = None) -> FlowResult:
-    if user_input is not None:
-        await self.async_set_unique_id(user_input[CONF_DEVICE_ID])
-        self._abort_if_unique_id_configured()
-        return self.async_create_entry(
-            title=user_input[CONF_NAME],
-            data=user_input,
-        )
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        if user_input is not None:
+            await self.async_set_unique_id(user_input[CONF_DEVICE_ID])
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(
+                title=user_input[CONF_NAME],
+                data=user_input,
+            )
 
-    return self.async_show_form(
-        step_id="user",
-        data_schema=STEP_USER_SCHEMA,
-        errors={},
-    )
+        return self.async_show_form(
+            step_id="user",
+            data_schema=STEP_USER_SCHEMA,
+            errors={},
+        )
